607afaf55fa81b27b7a76a60a64351049f948e42
Navaneeth Balaji <n.balaji@student.unsw.edu.au>
2020-10-07 12:04:32 +0800

    Setup authentication flow

---
+++ src/authenticate.tsx
@@ -0,0 +1,26 @@
+import { Session } from "inspector";
+import React, { Component } from "react";
+import App from "./App";
+import logo from "./logo.svg"
+
+export default function() {
+
+    let token  = window.location.hash.substr(1);
+    if(token) {
+        const o = Object.fromEntries(new URLSearchParams(token));
+        return o.access_token;
+    } else {
+        redirectToSpotify();
+    }
+}
+
+function redirectToSpotify() {
+
+    const authEndpoint = 'https://accounts.spotify.com/authorize';
+    const clientId = process.env.REACT_APP_CLIENT_ID;
+    const redirectURI = `${window.location.protocol}//${window.location.host}`;
+    //let sess_id = /SESS\w*ID=([^;]+)/i.test(document.cookie) ? RegExp.$1 : false;
+    //const state = hash(sess_id)
+    let query = `client_id=${clientId}&redirect_uri=${redirectURI}&response_type=token&`
+    window.location.href = `${authEndpoint}?${query}`;
+}
