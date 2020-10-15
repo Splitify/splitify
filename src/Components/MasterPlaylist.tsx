9adb8aa76e6d96520b390355aa669cab07790979
Cary Guan <z5160345@student.unsw.edu.au>
2020-10-15 21:15:47 +1100

    cleaning code

--- src/Components/MasterPlaylist.tsx
+++ src/Components/MasterPlaylist.tsx
@@ -8,7 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Track from './Track';
-import { PlaylistObj, PlaylistTrackObj, TrackObj } from '../helpers/interfaces';
+import { PlaylistObj, PlaylistTrackObj} from '../helpers/interfaces';
 
 const useStyles = makeStyles({
   table: {
