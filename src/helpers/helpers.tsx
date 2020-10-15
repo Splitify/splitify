11cb2acac5aa24822a2917fb8c75d3cb28255058
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-16 00:31:41 +1100

    Start getUserProfile
    Should be called after auth success
    @thenavz6

--- src/helpers/helpers.tsx
+++ src/helpers/helpers.tsx
@@ -1,3 +1,22 @@
 export function getPlaylist() {
     
+export async function getUserProfile (api: any) {
+  // Get current user data
+  api
+    .getMe()
+    .then(
+      ({
+        display_name,
+        id,
+        images
+      }: {
+        display_name: string
+        id: string
+        images: Array<any>
+      }) => {
+        console.log(display_name)
+        console.log(id)
+        console.log(images)
+      }
+    )
 }
