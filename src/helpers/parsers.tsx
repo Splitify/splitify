0a885a691b81053b7a382a79a970f80d5120ddb9
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-16 18:31:03 +1100

    Add profile picture URL into User

--- src/helpers/parsers.tsx
+++ src/helpers/parsers.tsx
@@ -6,10 +6,11 @@ import { User, Playlist, Track } from '../types'
 //   }
 // }
 
-export function parseUserJSON ({ id, display_name }: any): User {
+export function parseUserJSON ({ id, display_name, images }: any): User {
   return {
     id,
-    display_name
+    display_name,
+    image: images && images.length ? images[0].url : null
   }
 }
 
