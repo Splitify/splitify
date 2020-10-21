3b2df8d122bbe18710d450e511e6af53cf9c415f
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-22 02:36:44 +1100

    Use (async) getPlaylist()

--- src/helpers/helpers.tsx
+++ src/helpers/helpers.tsx
@@ -33,41 +33,16 @@ async function* getPlaylistsRawGen() {
   } while (offset < total)
 }
 
-async function getAlbums(ids: string[], ans: Album[]): Promise<Album[]> {
-  if (!ids.length) return ans;
-  const albums = await api.getAlbums(ids.splice(0, 20)); // There is a limit of 20 per call
-  return (await getAlbums(ids, ans)).concat(albums.albums.map((a: any) => parseAlbumJSON(a)));
-}
-
-async function getArtists(ids: string[], ans: Artist[]): Promise<Artist[]> {
-  if (!ids.length) return ans;
-  const artists = await api.getArtists(ids.splice(0, 50)); // There is a limit of 50 per call
-  return (await getArtists(ids, ans)).concat(artists.artists.map((a: any) => parseArtistJSON(a)));
-}
-
-const unique = (value: any, index: any, self: string | any[]) => self.indexOf(value) === index;
-
-export async function getPlaylist(playlistId: string = "1qLoOFlMBRMBTeSnQ5guuc"): Promise<Playlist> {
-  let playlist = await api.getPlaylist(playlistId) as any;
-
-  // TODO: handle paging
-
-  const album_ids: string[] = playlist.tracks.items.map((t: any) => t.track.album.id);
-  const track_ids: string[] = playlist.tracks.items.map((t: any) => t.track.id);
-  const artist_ids: string[] = playlist.tracks.items.map((t: any) => t.track.artists.map((a: any) => a.id)).flat().filter(unique);
-  const albums = await getAlbums(album_ids, []);
-  const features = (await api.getAudioFeaturesForTracks(track_ids)).audio_features.map(parseFeaturesJSON);
-  const artists = await getArtists(artist_ids, []);
-
-  playlist.tracks = await Promise.all(playlist.tracks.items.map(async (t: any) => {
-    t.track.album = albums.find((a: Album) => a.id === t.track.album.id);
-    t.track.features = features.find((f: Features) => f.id === t.track.id);
-    t.track.artists = t.track.artists.map((a: any) => artists.find((b: Artist) => a.id === b.id));
-    return await parseTrackJSON(t.track);
-  }));
-
-  await console.log("Retrieved Playlist:", playlist)
-  return await parsePlaylistJSON(playlist);
+export async function getPlaylist (
+  playlistId: string,
+  expand: boolean = false
+): Promise<Playlist> {
+  return parsePlaylistJSON(
+    await api.getPlaylist(playlistId, {
+      fields: '(!tracks)'
+    }),
+    expand
+  )
 }
 
 export async function getUserProfile(): Promise<User> {
