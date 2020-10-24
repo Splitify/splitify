996cb420e2318bf2399f6ae22fe4b7c47d789aee
Navaneeth Balaji <n.balaji@student.unsw.edu.au>
2020-10-24 10:26:37 +0800

    Updated Audio Feature slider component

--- src/components/AudioFeatureSlider.tsx
+++ src/components/AudioFeatureSlider.tsx
@@ -1,34 +1,41 @@
 import React , { useState } from 'react';
 import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
 
 export default function AudioFeatureSlider(props: {
-
+    feature_name: string,
+    feature_value: Number[],
+    giveFeaturesToPlaylist: (feature: Number[]) => void,
 }) {
 
-    function handleChange (value:any) {
+    const handleChange = (event: any, value: number | number[]) => {
         console.log('changing a value')
         console.log(value)
-        if (typeof(value) === 'number'){
-          return [value,value]
-        }else{
-          return value
-        }
+        setSliderVal(value as number[]);
+        props.giveFeaturesToPlaylist(value as number[]);
+    };
+
+    function valuetext(value:Number) {
+        return `${value}`;
     }
 
-    const [SliderVal, setSliderVal] = useState
+    const [SliderVal, setSliderVal] = useState(props.feature_value);
 
     return(
-        <Slider
-            onChangeCommitted={(event, value) =>
-                setSliderVal(handleChange(value))
-            }
-            aria-label = "tempo"
-            orientation="vertical"
-            valueLabelDisplay="auto"
-            aria-labelledby="range-slider"
-            defaultValue = {SliderVal.map((num) => Number(num))}
-            getAriaValueText={valuetext}
-        />
+        <div>
+            <Typography id="range-slider" gutterBottom>
+                {props.feature_name}
+            </Typography>
+            <Slider
+                onChangeCommitted={handleChange}
+                aria-label = {props.feature_name}
+                orientation = "horizontal"
+                valueLabelDisplay = "auto"
+                aria-labelledby = "range-slider"
+                defaultValue = {SliderVal.map((num) => Number(num))}
+                getAriaValueText = {valuetext}
+            />
+        </div>
     )
 
 }
