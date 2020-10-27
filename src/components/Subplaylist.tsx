97711a905ce226c17acd394af4953daab570e3d4
carlinwilliamson <carlin.williamson1@gmail.com>
2020-10-27 20:12:10 +1100

    restrict menu to unused features

--- src/components/Subplaylist.tsx
+++ src/components/Subplaylist.tsx
@@ -27,22 +27,25 @@ import EditPlaylistNameDialog from './EditPlaylistNameDialog'
 
 
 export function FeatureMenu(props: {
-  giveOptionToPlaylist : (option: string) => void
+  giveOptionToPlaylist: (option: string) => void
+  alreadyActive: string[]
 }) {
+  const options = ['Acousticness', 'Danceability', 'Energy',
+    'Instrumentalness', 'Liveness', 'Speechiness', 'Valence'].filter(o => !props.alreadyActive.includes(o));
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const handleClick = (event:any) => {
+  const handleClick = (event: any) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleChange = (selected:any) => {
+  const handleChange = (selected: any) => {
     props.giveOptionToPlaylist(selected)
   }
   return (
-    <div> 
-    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
         Add Audio Feature
       </Button>
       <Menu
@@ -52,15 +55,9 @@ export function FeatureMenu(props: {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={() => handleChange({name:'Acousticness', min:10, max:90})}>Acousticness</MenuItem>
-        <MenuItem onClick={() => handleChange({name:'Danceability', min:10, max:90})}>Danceability</MenuItem>
-        <MenuItem onClick={() => handleChange({name:'Energy', min:10, max:90})}>Energy</MenuItem>
-        <MenuItem onClick={() => handleChange({name:'Instrumentalness', min:10, max:90})}>Instrumentalness</MenuItem>
-        <MenuItem onClick={() => handleChange({name:'Liveness', min:10, max:90})}>Liveness</MenuItem>
-        <MenuItem onClick={() => handleChange({name:'Speechiness', min:10, max:90})}>Speechiness</MenuItem>
-        <MenuItem onClick={() => handleChange({name:'Valence', min:10, max:90})}>Valence</MenuItem>
+        {options.map(o => (<MenuItem onClick={() => handleChange({ name: o, min: 10, max: 90 })}>{o}</MenuItem>))}
       </Menu>
-      </div>
+    </div>
   );
 }
 
@@ -87,7 +84,7 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export default function Subplaylist (props: {
+export default function Subplaylist(props: {
   source: PlaylistObj
   playlist: PlaylistObj
   genres: string[]
@@ -96,78 +93,77 @@ export default function Subplaylist (props: {
   const classes = useStyles()
   const [selectedGenres, setSelectedGenres] = useState<string[]>([])
 
-  const [sliders, setSliders] = useState<{name:string, min:number, max:number}[]>([]); 
+  const [sliders, setSliders] = useState<{ name: string, min: number, max: number }[]>([]);
 
   const deleteSlider = (id: String) => {
     console.log("Deleting slider ", id);
     setSliders(sliders.filter(k => k.name !== id));
-}
+  }
 
-const updateSlider = (id:String, range:Number[]) => {
-  setSliders(
-    sliders.map(
-      el => el.name===id? {...el,min : Number(range[0]), max:Number(range[1])}:el
+  const updateSlider = (id: String, range: Number[]) => {
+    setSliders(
+      sliders.map(
+        el => el.name === id ? { ...el, min: Number(range[0]), max: Number(range[1]) } : el
+      )
     )
-  )    
-  
-}
-const getFeaturesFromSlider = (name:string, incomingFeatures: Number[]) => {
-  updateSlider(name, incomingFeatures)
-  
-}
 
-const getOptionFromMenu = (option:any) =>
-{ 
-  var found = false;
-  for(var i = 0; i < sliders.length; i++) {
-    if (sliders[i].name === option.name) {
+  }
+  const getFeaturesFromSlider = (name: string, incomingFeatures: Number[]) => {
+    updateSlider(name, incomingFeatures)
+
+  }
+
+  const getOptionFromMenu = (option: any) => {
+    var found = false;
+    for (var i = 0; i < sliders.length; i++) {
+      if (sliders[i].name === option.name) {
         found = true;
         break;
+      }
+    }
+    if (!found) {
+      setSliders([...sliders, option])
     }
   }
-  if (!found){
-    setSliders([...sliders,option])
-  }
-}
-const TrackInRange = (track : TrackObj) : boolean => {
-  var found = true;
-  sliders.map((slider) => {
-    if (track.features){
-      if (slider.name === 'Acousticness' && (track.features.acousticness < slider.min/100  || track.features.acousticness > slider.max/100)){
-        found=false;
+  const TrackInRange = (track: TrackObj): boolean => {
+    var found = true;
+    sliders.map((slider) => {
+      if (track.features) {
+        if (slider.name === 'Acousticness' && (track.features.acousticness < slider.min / 100 || track.features.acousticness > slider.max / 100)) {
+          found = false;
+        }
+        if (slider.name === 'Danceability' && (track.features.danceability < slider.min / 100 || track.features.danceability > slider.max / 100)) {
+          found = false;
+        }
+        if (slider.name === 'Energy' && (track.features.energy < slider.min / 100 || track.features.energy > slider.max / 100)) {
+          found = false;
+        }
+        if (slider.name === 'Instrumentalness' && (track.features.instrumentalness < slider.min / 100 || track.features.instrumentalness > slider.max / 100)) {
+          found = false;
+        }
+        if (slider.name === 'Liveness' && (track.features.liveness < slider.min / 100 || track.features.liveness > slider.max / 100)) {
+          found = false;
+        }
+        // if (slider.name === 'Loudness' && track.features.loudness < slider.min/100 || track.features.loudness > slider.max/100){
+        //   found=false;
+        // }
+        if (slider.name === 'Speechiness' && (track.features.speechiness < slider.min / 100 || track.features.speechiness > slider.max / 100)) {
+          found = false;
+        }
+        // if (slider.name === 'Tempo' && track.features.tempo < slider.min/100 || track.features.tempo > slider.max/100){
+        //   found=false;
+        // }
+        if (slider.name === 'Valence' && (track.features.valence < slider.min / 100 || track.features.valence > slider.max / 100)) {
+          found = false;
+        }
       }
-      if (slider.name === 'Danceability' && (track.features.danceability < slider.min/100  || track.features.danceability > slider.max/100)){
-        found=false;
-      }
-      if (slider.name === 'Energy' && (track.features.energy < slider.min/100 || track.features.energy > slider.max/100)){
-        found=false;
-      }
-      if (slider.name === 'Instrumentalness' && (track.features.instrumentalness < slider.min/100 || track.features.instrumentalness > slider.max/100)){
-        found=false;
-      }
-      if (slider.name === 'Liveness' && (track.features.liveness < slider.min/100 || track.features.liveness > slider.max/100)){
-        found=false;
-      }
-      // if (slider.name === 'Loudness' && track.features.loudness < slider.min/100 || track.features.loudness > slider.max/100){
-      //   found=false;
-      // }
-      if (slider.name === 'Speechiness' && (track.features.speechiness < slider.min/100 || track.features.speechiness > slider.max/100)){
-        found=false;
-      }
-      // if (slider.name === 'Tempo' && track.features.tempo < slider.min/100 || track.features.tempo > slider.max/100){
-      //   found=false;
-      // }
-      if (slider.name === 'Valence' && (track.features.valence < slider.min/100 || track.features.valence > slider.max/100)){
-        found=false;
-      }
-  }
-  return found;
-  })
-  
-  
+      return found;
+    })
 
-  return found;
-}
+
+
+    return found;
+  }
   // TODO: Maybe put genres for each genre
   const TrackCorrectGenre = (track: TrackObj): boolean => {
     for (let artist of track.artists) {
@@ -207,36 +203,36 @@ const TrackInRange = (track : TrackObj) : boolean => {
       </Dialog>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="simple table">
-            <TableHead>
-              {sliders.map(p => (
-                <TableRow> 
-                <TableCell> 
-              <AudioFeatureSlider feature_name = {p.name} feature_value = {[p.min,p.max]} delete = {() => deleteSlider(p.name)} giveFeaturesToPlaylist = {getFeaturesFromSlider}/>
-              </TableCell>
-              <Button variant="contained" color="secondary" onClick = {() =>deleteSlider(p.name)}startIcon={<DeleteIcon />}>
-              </Button>
-              </TableRow>
-            ))}
+          <TableHead>
+            {sliders.map(p => (
               <TableRow>
                 <TableCell>
-                  {props.playlist.name}
-                  <IconButton onClick={() => setEditDialogOpen(true)}>
-                    <EditIcon />
-                  </IconButton>
+                  <AudioFeatureSlider feature_name={p.name} feature_value={[p.min, p.max]} delete={() => deleteSlider(p.name)} giveFeaturesToPlaylist={getFeaturesFromSlider} />
+                </TableCell>
+                <Button variant="contained" color="secondary" onClick={() => deleteSlider(p.name)} startIcon={<DeleteIcon />}>
+                </Button>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell>
+                {props.playlist.name}
+                <IconButton onClick={() => setEditDialogOpen(true)}>
+                  <EditIcon />
+                </IconButton>
               </TableCell>
               <TableCell>
-                  <Button variant="contained" color="secondary" onClick={() => props.onDelete && props.onDelete(props.playlist)} startIcon={<DeleteIcon />}>
-                    Delete
+                <Button variant="contained" color="secondary" onClick={() => props.onDelete && props.onDelete(props.playlist)} startIcon={<DeleteIcon />}>
+                  Delete
                   </Button>
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
 
-            <TableRow> 
-              <TableCell colSpan = {2}>
-            <FeatureMenu giveOptionToPlaylist = {getOptionFromMenu}/>
-            </TableCell>
+            <TableRow>
+              <TableCell colSpan={2}>
+                <FeatureMenu giveOptionToPlaylist={getOptionFromMenu} alreadyActive={sliders.map(el => el.name)} />
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell colSpan={2}>
@@ -273,15 +269,15 @@ const TrackInRange = (track : TrackObj) : boolean => {
                 />
               </TableCell>
             </TableRow>
-            {tracks.filter(TrackCorrectGenre ).map(track => (
-              
+            {tracks.filter(TrackCorrectGenre).map(track => (
+
               <TableRow key={track.id}>
                 <TableCell colSpan={2} component='th' scope='row'>
                   <Track track={track} />
                 </TableCell>
               </TableRow>
             ))}
-            </TableBody>
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
