74dd9da1bb3f5ee574122a1eea2184cae36c7d38
carlinwilliamson <carlin.williamson1@gmail.com>
2020-10-30 00:47:40 +1100

    added graphs and images

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -4,7 +4,11 @@ import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { Track as TrackObj } from '../types'
-import { CardMedia } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import ExplicitIcon from '@material-ui/icons/Explicit';
+import {
+  BarChart, Bar, Cell, XAxis, YAxis
+} from 'recharts';
 
 const strArrayToEngligh = (arr: string[]) => {
   const re = /(.*), (\w+)/;
@@ -16,6 +20,14 @@ const numToNaturalTime = (n: Number) => {
   return `${date.getMinutes()} min ${date.getSeconds()} sec`
 }
 
+const COLOURS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const EXCLUDED_FEATURES = [ // TODO: This should be shared with Zach's feature code
+  "duration_ms",
+  "tempo",
+  "mode",
+  "time_signature",
+  "key"];
+
 export default function Track(props: { track: TrackObj }): JSX.Element {
 
   const useStyles = makeStyles((theme) => ({
@@ -24,6 +36,17 @@ export default function Track(props: { track: TrackObj }): JSX.Element {
     },
     paper: {
       padding: theme.spacing(1),
+    },
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '80%',
+      maxHeight: '80%',
+    },
+    container: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(12, 1fr)',
+      gridGap: theme.spacing(3),
     },
   }));
 
@@ -37,13 +60,38 @@ export default function Track(props: { track: TrackObj }): JSX.Element {
   const handlePopoverClose = () => {
     setAnchorEl(null);
   };
-    
-  
+
+
   const artistEnglish = strArrayToEngligh(props.track.artists.map(a => a.name));
-  const albumEnglish = props.track.album?.name ?? "";
   const genresEnglish = strArrayToEngligh(Array.from(new Set(props.track.artists.map(a => a.genres).flat())));
   const lengthEnglish = numToNaturalTime(props.track.duration_ms);
-  
+
+  const data = Object.entries(props.track.features ?? {}).map(([k, v]) => {
+    return { name: k, value: v };
+  }).filter(v => !EXCLUDED_FEATURES.includes(v.name))
+
+
+  const albumGrid = (
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <img className={classes.img} alt="complex" src={props.track.album?.image.toString()} />
+      </Grid>
+      <Grid item xs={6}>
+        <Typography gutterBottom variant="subtitle1">
+          {props.track.album?.name} {props.track.explicit ? <ExplicitIcon /> : ""}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          Released: {props.track.album?.release_date.toDateString()}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Track: {props.track.track_number} of {props.track.album?.total_tracks}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+
+  var colourIndex = -1;
+
   const open = Boolean(anchorEl);
   return (
     <React.Fragment>
@@ -59,6 +107,7 @@ export default function Track(props: { track: TrackObj }): JSX.Element {
           {props.track.name}
         </Typography>
         <Popover
+          style={{ width: '40%' }}
           id="mouse-over-popover"
           className={classes.popover}
           classes={{
@@ -78,27 +127,38 @@ export default function Track(props: { track: TrackObj }): JSX.Element {
           disableRestoreFocus
         >
           <Typography>
-            Artists: {artistEnglish}
-            <br/>
-            {albumEnglish.length > 0 ? "Album: " + albumEnglish : ""}
-            <br/>
-            {genresEnglish.length > 0 ? "Genres: " + genresEnglish : ""}
-            <br />
-            Track: {props.track.track_number} of {props.track.album?.total_tracks}
-            <br />
-            Popularity: {props.track.popularity}
-            <br />
-            Length: {lengthEnglish}
-            <br />
-            Explicit: {props.track.explicit}
-            <br />
-            {/* Release date: {props.track.album?.release_date} */}
-            <br />
-            Features: {props.track.features?.toString()}
-            
           </Typography>
+          <Grid container spacing={2}>
+            {props.track.album === undefined ? "" : albumGrid}
+            <Grid item>
+              Artists: {artistEnglish}
+            </Grid>
+            <Grid item>
+              {genresEnglish.length > 0 ? "Genres: " + genresEnglish : ""}
+            </Grid>
+            <Grid item>
+              Popularity: {props.track.popularity}
+            </Grid>
+            <Grid item>
+              Length: {lengthEnglish}
+            </Grid>
+            <Grid item>
+              <BarChart width={400} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 50 }} layout={"vertical"}
+              >
+                <XAxis type={"number"} domain={[0, 1]} hide />
+                <YAxis type={"category"} dataKey={"name"} width={100} axisLine={false} tickSize={0} />
+                <Bar dataKey="value">
+                  {
+                    data.map((entry, index) => {
+                      colourIndex = colourIndex + 1;
+                      return <Cell fill={COLOURS[colourIndex % COLOURS.length]} />;
+                    })
+                  }
+                </Bar>
+              </BarChart>
+            </Grid>
+          </Grid>
         </Popover>
-        <CardMedia image={props.track.album?.image.toString()} />
       </div>
     </React.Fragment>
   )
