6937c1d35397d2bb30c9368ce79eefc5f38a99ce
carlinwilliamson <carlin.williamson1@gmail.com>
2020-11-05 00:17:18 +1100

    caching and whitelist

--- src/helpers/parsers.tsx
+++ src/helpers/parsers.tsx
@@ -29,7 +29,7 @@ async function fetchLFM(
 
 const GenreAccumulator = new CachingAccumulumatorinator<
   any
->('genres', 1, async id => (await fetchLFM("http://ws.audioscrobbler.com/2.0/?" + id)))
+>('genres', 1, async id => ([await fetchLFM("http://ws.audioscrobbler.com/2.0/?" + id)]))
 
 const TrackAccumulator = new CachingAccumulumatorinator<
   SpotifyApi.TrackObjectFull
@@ -48,7 +48,7 @@ const ArtistAccumulator = new CachingAccumulumatorinator<
   SpotifyApi.ArtistObjectFull
 >('artists', 50, async ids => (await api.getArtists(ids))['artists'])
 
-export async function parseTrackJSON (
+export async function parseTrackJSON(
   json: SpotifyApi.TrackObjectFull,
   expand?: boolean
 ): Promise<Track> {
@@ -84,12 +84,12 @@ export async function parseTrackJSON (
         this.album = await parseAlbumJSON(
           await AlbumAccumulator.request(json.album.id)
         )
-        
+
         this.genres = await parseGenres(
-          await fetchLFM("http://ws.audioscrobbler.com/2.0/?" + ToURLParam(json.name, json.artists[0].name)),
-          // await GenreAccumulator.requestURL(this.id, ToURLParam(json.name, json.artists[0].name), true),
+          // await fetchLFM("http://ws.audioscrobbler.com/2.0/?" + ToURLParam(json.name, json.artists[0].name)),
+          await GenreAccumulator.requestURL(this.id, ToURLParam(json.name, json.artists[0].name), true),
+          this.artists,
           this.album,
-          this.artists
         )
         resolve(this)
       })
@@ -100,28 +100,37 @@ export async function parseTrackJSON (
   return expand ? await track.expand() : track
 }
 
-export function parseGenres (
+const WHITELIST = ["hi", "lo", "fi", "acid", "acousmatic", "acoustic", "adult", "africa", "african", "afro", "age", "aggrotech", "aka", "along", "alternative", "alternativo", "ambient", "american", "americana", "anatolian", "and", "anime", "anison", "anti", "apala", "arab", "arena", "argentine", "art", "asia", "asian", "australia", "australian", "austropop", "avant", "avantgarde", "axé", "bachata", "backbeat", "background", "baggy", "baithak", "bakersfield", "baladas", "balearic", "balkan", "ballad", "ballet", "baltimore", "band", "bands", "bap", "barbershop", "baroque", "bass", "bassline", "beat", "bebop", "benga", "berlin", "big", "bikutsi", "bit", "bitpop", "black", "bleeps", "blue", "bluegrass", "blues", "body", "bolero", "boleros", "bongo", "boogie", "bop", "bossa", "bounce", "bouncy", "boy", "brasileira", "brazilian", "breakbeat", "breakcore", "breaks", "breakstep", "brega", "brick", "british", "britpop", "britpunk", "broken", "brostep", "bubblegum", "bultrón", "cajun", "calypso", "canadian", "cantata", "cantique", "cantopop", "cape", "cappella", "caribbean", "carioca", "ccm", "chamber", "chanson", "chant", "chanukah", "chap", "chicago", "chicano", "chicha", "children’s", "chill", "chillstep", "chillwave", "chimurenga", "chiptune", "choral", "choro", "christian", "christmas", "christmas:", "chutney", "cinema", "cities", "city", "classic", "classical", "close", "club", "coast", "cock", "coding", "coldwave", "college", "comedy", "commercial", "compas", "composition", "computer", "concerto", "concrète", "conjunto", "conscious", "contemporary", "cool", "core", "country", "county", "coupé", "cowboy", "cowpunk", "criolla", "crossover", "crunk", "crunkcore", "crust", "cumbia", "cybergrind", "dance", "dancehall", "dansband", "dark", "darkcore", "darkside", "darkstep", "death", "deathcore", "décalé", "deep", "delta", "detroit", "deutsche", "digital", "dirt", "dirty", "disco", "disney", "diva", "dixieland", "djent", "dnb", "doo", "doom", "doomcore", "downtempo", "dream", "driving", "drone", "drum", "drumfunk", "drum’n’bass", "drumstep", "dub", "dubstep", "dubstyle", "dubtronica", "dutch", "early", "east", "easter", "easy", "electric", "electro", "electroacoustic", "electroclash", "electronic", "electronica", "electronicore", "electronics", "electropop", "electropunk", "electroswing", "elevator", "emotional", "english", "enka", "environmental", "español", "ethereal", "ethio", "ethnic", "eurobeat", "eurodance", "europop", "exercise", "experimental", "expressionist", "fado", "fann", "female", "fiddle", "field", "fijiri", "filk", "fitness", "flamenco", "flava", "florida", "flow", "folk", "folktronica", "foreign", "forró", "franco", "freak", "free", "freestyle", "french", "frevo", "fuji", "full", "funk", "funky", "furniture", "fusion", "future", "futurepop", "gabber", "game", "gana", "gangsta", "garage", "garde", "genge", "german", "ghetto", "ghettotech", "glam", "glitch", "goa", "golden", "gospel", "goth", "gothic", "gregorian", "grime", "grind", "grindcore", "groove", "grosso", "grunge", "gulf", "gypsy", "hair", "halloween", "happy", "hard", "hardbag", "hardcore", "hardstep", "hardstyle", "harmonica", "harmony", "härte", "healing", "heavy", "hellbilly", "high", "highlife", "hill", "hip", "hiplife", "hokum", "holiday", "holiday:", "hong", "honky", "hop", "horrorcore", "house", "housetribal", "huayno", "hyphy", "idm", "illbient", "impressionist", "improvisation", "indie", "indietronica", "industrial", "inspirational", "instrumental", "intelligent", "invasion", "iranian", "isicathamiya", "isolationism", "italo", "jack", "jackin", "jam", "jangle", "japanese", "japanoise", "jazz", "jersey", "jingles", "jit", "jùjú", "jump", "jumpstyle", "jungle", "jungle:", "kansas", "kapuka", "karaoke", "kawaii", "kayokyoku", "kayōkyoku", "khaliji", "kizomba", "kong", "korean", "kuduro", "kwaito", "kwela", "laïkó", "lambada", "laptronica", "latin", "latino", "levenslied", "liquid", "listening", "live", "liwa", "louisiana", "lounge", "love", "lovers", "low", "lowercase", "lubbock", "lullabies", "lyrical", "madchester", "mafioso", "mainstream", "makina", "makossa", "maloya", "mambo", "mandopop", "maracatu", "march", "mariachi", "marrabenta", "mass", "math", "mathcore", "mbalax", "mbaqanga", "mbube", "medieval", "meditation", "memphis", "merengue", "merenrap", "méringue", "metal", "metalcore", "metall", "mex", "mexican", "mexicano", "middle", "midwest", "minimal", "minimalism", "modal", "modern", "moombahton", "morna", "motorpop", "motown", "motswako", "movie", "museve", "music", "música", "musicals", "musique", "nashville", "nature", "nederpop", "neo", "neoclassical", "neofolk", "neotraditional", "nerdcore", "neue", "neurofunk", "new", "nintendocore", "noise", "nortec", "northern", "note", "nova", "novelty", "nrg", "nuevo", "old", "oldschool", "onkyokei", "opera", "oratorio", "orchestral", "organum", "oriented", "original", "orlean", "other", "out", "outlaw", "pagan", "pagode", "palm", "parody", "piano", "piedmont", "pirate", "poetry", "pop", "popera", "popular", "portuguese", "post", "power", "praise", "prog", "progressive", "psybreaks", "psychedelic", "psychobilly", "psyprog", "psytrance", "punk", "punkabilly", "punta", "qawwali", "quartet", "quiet", "ragga", "raggacore", "raggamuffin", "ragtime", "raï", "raíces", "raison", "ranchera", "rap", "rave", "reactionary", "recording", "red", "reggae", "reggaeton", "regional", "regstep", "relaxation", "religious", "renaissance", "requiem", "revival", "rhythm", "road", "rock", "rockabilly", "rocksteady", "roll", "romantic", "romanticism", "roots", "russian", "sakara", "salsa", "samba", "sambass", "sawt", "schlager", "school", "schranz", "score", "scouse", "sega", "seggae", "semba", "sertaneja", "sertanejo", "shoegaze", "shouter", "sing", "singer", "ska", "skool", "skweee", "sludge", "smooth", "soca", "soft", "son", "sonata", "song", "songwriter", "sophisti", "soukous", "soul", "sound", "soundscape", "soundtrack", "south", "southern", "space", "spanish", "spazzcore", "speed", "speedcore", "spoken", "stand", "standards", "steampunk", "step", "stoner", "stories", "storm", "stream", "string", "sung", "sunshine", "suomisaundi", "surf", "swamp", "swing", "symphonic", "symphony", "synth", "synthcore", "synthpop", "synthpunk", "taarab", "taiwanese", "tanbura", "tango", "tape", "tech", "technical", "techno", "technopop", "techstep", "tecno", "tecnobrega", "teen", "tejano", "tekno", "terrorcore", "tex", "texas", "thanksgiving", "themes", "third", "thrash", "timba", "time", "típico", "tone", "tonk", "toytown", "trad", "traditional", "trance", "trap", "travel", "trip", "tropical", "tropicalia", "truck", "tunes", "turbofolk", "turkish", "turntablism", "twin", "twoubadou", "underground", "uplifting", "urban", "vandeville", "vaudeville", "video", "viking", "vispop", "vocal", "wave", "wedding", "west", "western", "wine", "witch", "wonky", "wop", "word", "workout", "world", "worship", "yorkshire", "zouglou", "zouk", "zydeco"]
+
+export function parseGenres(
   lm: any,
-  album: Album,
-  artists: Artist[]
+  artists: Artist[],
+  album?: Album,
 ) {
-  const artistNames = artists.map((a:Artist) => a.name.toLowerCase());
-  let genres = lm.toptags?.tag.filter((t: any) => t.count > 50).map((t: any) => t.name.toLowerCase()) ?? [];
-  genres.splice(3,99)
+  const artistNames = artists.map((a: Artist) => a.name.toLowerCase());
+  let genres = lm.toptags?.tag
+    .filter((t: any) => t.count > 50 && t.name.length < 20)
+    .map((t: any) => t.name.toLowerCase())
+    .filter((s: string) => {
+      return s.split(" ").some((g: string) => WHITELIST.includes(g))
+        || s.split("-").some((g: string) => WHITELIST.includes(g))
+    }) ?? [];
+  genres.splice(3, 99); // Only keep the first three. The quality goes down quick on some songs
   genres = genres.filter((g: string) => !artistNames.includes(g));
-    
+
   if (genres.length === 0) {
-    genres = album.genres;
+    genres = album?.genres ?? [];
   }
-  
+
   if (genres.length === 0) {
     genres = artists.map((a: Artist) => a.genres).flat();
   }
   
+
   return genres;
 }
 
-export function parseFeaturesJSON (
+export function parseFeaturesJSON(
   json: SpotifyApi.AudioFeaturesResponse
 ): Features {
   return {
@@ -141,7 +150,7 @@ export function parseFeaturesJSON (
   }
 }
 
-export function parseArtistJSON (json: SpotifyApi.ArtistObjectFull): Artist {
+export function parseArtistJSON(json: SpotifyApi.ArtistObjectFull): Artist {
   return {
     followers: json.followers.total,
     genres: json.genres,
@@ -154,7 +163,7 @@ export function parseArtistJSON (json: SpotifyApi.ArtistObjectFull): Artist {
   }
 }
 
-export async function parseAlbumJSON (
+export async function parseAlbumJSON(
   json: SpotifyApi.AlbumObjectFull,
   expand?: boolean
 ): Promise<Album> {
@@ -189,7 +198,7 @@ export function parseUserJSON({ id, display_name, images }: any): User {
   }
 }
 
-export async function parsePlaylistJSON (
+export async function parsePlaylistJSON(
   json: SpotifyApi.PlaylistObjectFull,
   expand?: boolean,
   expandTrack?: boolean
