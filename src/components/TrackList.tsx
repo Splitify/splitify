38bc69a8c1da739c8dbc6debc42e6c2279e8de8f
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-05 17:17:29 +1100

    Integrate react-beautiful-dnd with react-window

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import { Droppable } from 'react-beautiful-dnd'
-import { Track } from '../types'
+import { Track as TrackObj } from '../types'
 import TrackEntry from './TrackEntry'
+import Track from './Track'
 import { VariableSizeList as List } from 'react-window'
 
-export default function (props: { id?: string; tracks: Track[], isDropDisabled?: boolean, isDragDisabled?: boolean, isDragClone?: boolean, component: React.ElementType, childComponent: React.ElementType}) {
+export default function (props: { id?: string; tracks: TrackObj[], isDropDisabled?: boolean, isDragDisabled?: boolean, isDragClone?: boolean, component: React.ElementType, childComponent: React.ElementType}) {
   const Wrapper = props.component;
+
+  const EntryInvariant = React.memo(({ data, index, style }: any) => (
+    data[index] && <TrackEntry
+      key={data[index].id}
+      parent={props.id}
+      track={data[index]}
+      index={index}
+      isDragDisabled={props.isDragDisabled}
+      style={style}
+    />
+  ))
+
+  const TrackInvariant = (provided: any, snapshot: any, rubric: any) => (
+    <div
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      ref={provided.innerRef}
+    >
+      <Track track={props.tracks[rubric.source.index]} />
+    </div>
+  )
+
   return (
     <Droppable
       droppableId={props.id || 'unknown'}
       mode='virtual'
       isDropDisabled={props.isDropDisabled}
+      renderClone={TrackInvariant}
     >
       {(provided, snapshot) => (
         <List
@@ -18,21 +42,12 @@ export default function (props: { id?: string; tracks: Track[], isDropDisabled?:
           {...provided.droppableProps}
           innerElementType={Wrapper}
           height={400}
-          itemCount={props.tracks.length}
+          itemCount={props.tracks.length + (snapshot.isUsingPlaceholder ? 1 : 0)}
           itemData={props.tracks}
           itemSize={() => 60}
           width='100%'
         >
-          {({ data, index, style }) => (
-            <TrackEntry
-              key={data[index].id}
-              parent={props.id}
-              track={data[index]}
-              index={index}
-              isDragDisabled={props.isDragDisabled}
-              style={style}
-            />
-          )}
+          {EntryInvariant}
         </List>
         // {provided.placeholder}
       )}
