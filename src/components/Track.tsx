bfe0abf3e5434957877f2817ed87e22b487a7d64
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-06 01:29:44 +1100

    Disable text selection of track name
    https://www.pivotaltracker.com/story/show/175596227

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -25,6 +25,7 @@ export default function Track (props: {
           onMouseEnter={handlePopoverOpen}
           onMouseLeave={handlePopoverClose}
           align='center'
+          style={{'userSelect': 'none'}}
         >
           {props.track.name}
         </Typography>
