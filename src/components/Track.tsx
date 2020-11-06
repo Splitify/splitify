ac20aeb59ec9af17dbf908cafa2be9e4546fc8ac
zacharygilmour17 <z5162828@student.unsw.edu.au>
2020-11-06 14:29:40 +1100

    added song preview on click and onhoverclose
    added preview but i cannot get it to pause can only get it to play

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -8,15 +8,33 @@ export default function Track (props: {
   isDragging?: boolean
 }): JSX.Element {
   const [popupAnchor, setPopupAnchor] = React.useState(null)
+  var audio = new Audio(props.track.preview_url)
+  const[playing, setPlaying] = React.useState(false)
 
   const handlePopoverOpen = (event: any) => {
     props.isDragging || setPopupAnchor(event.currentTarget)
   }
 
   const handlePopoverClose = () => {
+    if (playing === true){
+      audio.pause()
+      setPlaying(false)
+      console.log('its pausing !')
+    }
     setPopupAnchor(null)
   }
-
+  const handlePreviewClick = () => {
+      if (playing === false){
+        audio.play()
+        setPlaying(true)
+        console.log('its playing !')
+      }else{
+        audio.pause()
+        setPlaying(false)
+        console.log('its pausing !')
+      }
+  }
+  
   return (
     <React.Fragment>
       <ListItemText>
@@ -24,6 +42,7 @@ export default function Track (props: {
           aria-haspopup='true'
           onMouseEnter={handlePopoverOpen}
           onMouseLeave={handlePopoverClose}
+          onClick = {handlePreviewClick}
           align='center'
         >
           {props.track.name}
