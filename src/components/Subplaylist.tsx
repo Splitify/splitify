c6aae679af3b19aa92b5f78c2197cdca7bd606e3
Cary Guan <z5160345@student.unsw.edu.au>
2020-11-10 12:13:13 +1100

    changed button

--- src/components/Subplaylist.tsx
+++ src/components/Subplaylist.tsx
@@ -191,7 +191,6 @@ export default function Subplaylist (props: {
     <div>
       <Button
         variant='contained'
-        color='primary'
         onClick={updateBlacklist}
       >
         Remove Selected Songs
