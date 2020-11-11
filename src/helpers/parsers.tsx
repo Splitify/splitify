b10d6c24c116f4eb52486032f491c395cd3374cb
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-11 19:12:25 +1100

    Skip track if it does not have a Spotify Track ID

--- src/helpers/parsers.tsx
+++ src/helpers/parsers.tsx
@@ -219,13 +219,12 @@ export async function parsePlaylistJSON(
 
         const Q = Queue({ autostart: true, concurrency: 1, timeout: 10 * 1000 })
 
-        // TODO: What if it's a local track?
-
         for await (let trackJSONBare of getPaginationRawGen(
           api.getPlaylistTracks,
           { fields: 'items.track.id,total' },
           this.id
         )) {
+          if (!trackJSONBare.track.id) continue;
           let track = TrackAccumulator.request(
             trackJSONBare['track']['id']
           ).then(async (data: SpotifyApi.TrackObjectFull) =>
