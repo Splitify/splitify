9c3c3195cf9d47f000f06b329f2fcabf38f941c3
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-13 19:09:31 +1100

    Tighten restriction to _just_ mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Router } from './routes/'
 import MobilePage from './routes/pages/mobile/'
-import { isMobile } from 'react-device-detect'
+import { isMobileOnly } from 'react-device-detect'
 
 export default function App () {
   return (
     <div className='App'>
-      { isMobile ? <MobilePage /> : <Router /> }
+      { isMobileOnly ? <MobilePage /> : <Router /> }
     </div>
   )
 }
