234bc8b79c113d5c4be6f8d397791b4067f56c41
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-13 19:06:38 +1100

    Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Router } from './routes/'
+import MobilePage from './routes/pages/mobile/'
 import { isMobile } from 'react-device-detect'
-import MobilePage from './routes/pages/mobile'
 
 export default function App () {
   return (
--- src/routes/pages/mobile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function() {
-    return <>
-      <h1>So about that...</h1>
-      <p>Splitify is not compatible on mobile devices. :(</p>
-      But you wouldn't want to experience all this greatness on a tiny screen anyway!
-    </>
-}
